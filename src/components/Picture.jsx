0d7bdc25fc46f312e6549fb128bb7340022b9553
gokcekemaloglu <gokce.kemaloglu@gmail.com>
2024-07-09 18:27:45 +0300

    updated

---
+++ src/components/Picture.jsx
@@ -0,0 +1,23 @@
+import dataa from "../helper/data";
+import "../styles/style.css";
+
+
+const Picture = ({photographer,src}) => {
+
+  return (
+    <div>
+
+        <div className="card">
+        <div className="picture">
+            <img src={src.large} alt="pictures" />
+        </div>
+            
+            <p>Photographer: {photographer}</p>
+        </div>
+
+        
+    </div>
+  )
+}
+
+export default Picture
